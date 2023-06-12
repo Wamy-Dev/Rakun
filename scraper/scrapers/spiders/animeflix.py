86188593ff5a812911f9c339c0f1c7911d1c2801
Wamy-Dev <25761442+Wamy-Dev@users.noreply.github.com>
2023-06-11 23:39:30 -0500

    animeflix scraper

---
+++ scraper/scrapers/spiders/animeflix.py
@@ -0,0 +1,33 @@
+import scrapy
+from scrapers.items import ScrapersItem
+import requests
+
+class AnimeflixSpider(scrapy.Spider):
+    name = 'animeflix'
+    allowed_domains = ['animeflix.live']
+    start_urls = ['https://animeflix.live/']
+    custom_settings = {
+        "ANIMEPIPELINE_ENABLED": True,
+    }
+
+    def parse(self, _):
+        # uses the api to get the anime list
+        totalPages = 5000
+        currentPage = 0
+
+        while currentPage <= totalPages:
+            animeData = requests.get(f"https://api.animeflix.live/series?page={currentPage}").json()
+            if animeData == []:
+                break
+            for anime in animeData:
+                title = anime["title"]["romaji"]
+                link = f"https://animeflix.live/watch/{anime['slug']}-episode-1"
+                animeItem = ScrapersItem()
+                animeItem["title"] = title
+                animeItem["link"] = link
+                animeItem["type"] = "Anime"
+                animeItem["source"] = "Animeflix"
+                yield animeItem
+            currentPage += 1
+
+
