e94f5d655e09c6b6face74c19cb48b593b6bb76d
Wamy-Dev <25761442+Wamy-Dev@users.noreply.github.com>
2023-06-11 23:10:56 -0500

    yugen scraper

---
+++ scraper/scrapers/spiders/yugen.py
@@ -0,0 +1,35 @@
+import scrapy
+from scrapers.items import ScrapersItem
+
+class YugenSpider(scrapy.Spider):
+    name = "yugen"
+    allowed_domains = ["yugen.to", "yugenanime.tv"]
+    start_urls = ["https://yugenanime.tv/discover/"]
+    custom_settings = {
+        "ANIMEPIPELINE_ENABLED": True,
+    }
+
+    def parse(self, response):
+        totalPages = response.css("li a.btn ::attr(href)")[1].get().split("=")[-1]
+        currentPage = response.css("li div.btn-default ::text").get()
+        if totalPages and currentPage:
+            if int(currentPage) == 1:
+                for pageNumber in range(2, int(totalPages) + 1):
+                    yield scrapy.Request(
+                        url=f"https://yugenanime.tv/discover/?page={pageNumber}")
+                    
+        animeList = response.css("a.anime-meta")
+        animeItem = ScrapersItem()
+        for anime in animeList:
+            title = anime.attrib["title"]
+            link = f"https://yugenanime.tv{anime.attrib['href']}"
+
+            animeItem["title"] = title
+            animeItem["link"] = link
+            animeItem["type"] = "Anime"
+            animeItem["source"] = "Yugen"
+
+            yield animeItem
+
+
+
