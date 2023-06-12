1b0ad9b1595673449caf9340e841c31109773763
Wamy-Dev <25761442+Wamy-Dev@users.noreply.github.com>
2023-06-11 22:58:00 -0500

    zoro spider

---
+++ scraper/scrapers/spiders/zoro.py
@@ -0,0 +1,35 @@
+import scrapy
+from scrapers.items import ScrapersItem
+
+class ZoroSpider(scrapy.Spider):
+    '''
+    Scrapes Anime from Zoro
+    '''
+    name = "zoro"
+    allowed_domains = ["zoro.to"]
+    start_urls = ["https://zoro.to/az-list"]
+    custom_settings = {
+        "ANIMEPIPELINE_ENABLED": True,
+    }
+
+    def parse(self, response):
+        totalPages = response.css("li.page-item ::attr(href)")[-1].get().split("=")[-1]
+        currentPage = response.css("li.page-item.active a ::text").get()
+        if totalPages and currentPage:
+            if int(currentPage) == 1:
+                for pageNumber in range(2, int(totalPages) + 1):
+                    yield scrapy.Request(
+                        url=f"https://zoro.to/az-list?page={pageNumber}")
+                    
+        animeList = response.css("a.dynamic-name")
+        animeItem = ScrapersItem()
+        for anime in animeList:
+            title = anime.attrib["title"]
+            link = f"https://zoro.to{anime.attrib['href']}"
+
+            animeItem["title"] = title
+            animeItem["link"] = link
+            animeItem["type"] = "Anime"
+            animeItem["source"] = "Zoro"
+
+            yield animeItem
