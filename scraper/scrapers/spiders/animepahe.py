2d9799c746206533bf7d1f1b6a60fdb75b709d78
Wamy-Dev <25761442+Wamy-Dev@users.noreply.github.com>
2023-06-11 23:21:05 -0500

    animepahe scraper

---
+++ scraper/scrapers/spiders/animepahe.py
@@ -0,0 +1,25 @@
+import scrapy
+from scrapers.items import ScrapersItem
+
+class AnimePaheSpider(scrapy.Spider):
+    name = "animepahe"
+    allowed_domains = ["animepahe.com", "animepahe.org", "animepahe.ru"]
+    start_urls = ["https://animepahe.ru/anime"]
+    custom_settings = {
+        "ANIMEPIPELINE_ENABLED": True,
+    }
+
+    def parse(self, response):
+        # Doesn't need to worry about pagination as it is all there, just hidden by javascript
+        animeList = response.css("div.tab-pane div.col-12 a")
+        animeItem = ScrapersItem()
+        for anime in animeList:
+            title = anime.attrib["title"]
+            link = f"https://animepahe.ru{anime.attrib['href']}"
+
+            animeItem["title"] = title
+            animeItem["link"] = link
+            animeItem["type"] = "Anime"
+            animeItem["source"] = "AnimePahe"
+
+            yield animeItem
