7218bec375ddd47dee97d080ea62ef8e4a129545
David <davidisadev@gmail.com>
2023-08-02 03:54:17 -0500

    updated gogoanime domains

--- scraper/scrapers/spiders/gogoanime.py
+++ scraper/scrapers/spiders/gogoanime.py
@@ -5,21 +5,21 @@ from bs4 import BeautifulSoup
 
 class GogoanimeSpider(scrapy.Spider):
     name = 'gogoanime'
-    allowed_domains = ["gogoanime.lu", "gogoanime.bid", "gogoanime.dk", "gogoanime.tel", "gogoanime.ar", "gogoanime.pe", "gogoanime.vc", "gogoanime.cl", "gogoanime.hu"]  # noqa: E501
-    start_urls = ["https://gogoanime.hu/anime-list.html"]
+    allowed_domains = ["gogoanime.cl", "gogoanime.hu", "gogoanime.gr", "gogoanime.movie", "gogoanime.ar", "gogoanime.dk", "gogoanime.ee", "gogoanime.lu", "gogoanime.so", "gogoanime.sk", "gogoanime.fi", "gogoanime.pe", "gogoanime.cm", "gogoanime.bid", "gogoanime.ai", "gogoanimehd.to"]  # noqa: E501
+    start_urls = ["https://gogoanimehd.to/anime-list.html"]
     custom_settings = {
         "ANIMEPIPELINE_ENABLED": True,
     }
 
     def parse(self, response):
-        prelimData = requests.get("https://gogoanime.hu/anime-list.html?page=5000")
+        prelimData = requests.get("https://gogoanimehd.to/anime-list.html?page=5000")
         soup = BeautifulSoup(prelimData.text, "html.parser")
         totalPages = soup.find("div", {"class": "pagination"}).find_all("a")[-1].text
         currentPage = response.css("li.selected a ::text").get()
         if totalPages and currentPage:
             if int(currentPage) == 1:
                 for pageNumber in range(2, int(totalPages) + 1):
-                    yield scrapy.Request(f"https://gogoanime.hu/anime-list.html?page={pageNumber}")
+                    yield scrapy.Request(f"https://gogoanimehd.to/anime-list.html?page={pageNumber}")
 
         animeList = response.css("ul.listing li a")
         animeItem = ScrapersItem()
@@ -27,7 +27,7 @@ class GogoanimeSpider(scrapy.Spider):
             title = anime.css("::text").get()
             link = anime.attrib["href"]
             animeItem["title"] = title.strip()
-            animeItem["link"] = {"Gogoanime":f"https://gogoanime.hu{link}"}
+            animeItem["link"] = {"Gogoanime":f"https://gogoanimehd.to{link}"}
             animeItem["type"] = "Anime"
             yield animeItem
 
